c0bcc0296c2c69f935f45088adceec19595f911a
sagar rajak <sagarrajak245@gmail.com>
2024-05-20 19:36:19 +0530

    third

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,28 @@ import React from 'react';
 // import { RiTwitterXLine } from "react-icons/ri";
 import logo from '../assets/dev.png';
 import SocialMediaLinks from './SocialMediaLinks';
+import Swapicons from './Swapicons';
 
-function Navbar( ) { 
+
+
+
+function Navbar( ) {    
+
+
+
+
   return (
     <nav className='   mb-20   flex  items-center justify-between    py-2'>
-<div className='flex flex-shrink-0 items-center  '>
+<div className='flex flex-shrink-0 items-center p '>
 <img src={logo} className='ml-2  h-14 w-auto pl-2' alt="logo" />
+<div className='ml-10 mt-2'><Swapicons/></div>
 </div> 
-<div className='m-6 h-auto item-center text-2xl flex gap-4 justify-center'>
-    <SocialMediaLinks/> 
-{/* 
-<button onClick={toggleTheme} className="ml-4 p-2 bg-gray-200 dark:bg-gray-700 rounded">
-          Toggle Theme
-        </button> */}
 
+
+
+<div className='m-6 h-auto item-center text-2xl flex gap-4 justify-center'>
+    <SocialMediaLinks/>   
+   
 </div>
 
 
